3dac5cef60a6beae8756409d89c11a6540e19b10
Andreas Billmeier (on testbox) <b@edevau.net>
2019-02-17 14:29:02 +0100

    add ESPHome Dashboard Patch from Vadim Efimov

---
+++ dev-embedded/esphome/files/esphome.conf.d
@@ -0,0 +1,10 @@
+# Copyright 1999-2019 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+# /etc/conf.d/esphome: configuration for /etc/init.d/esphome
+
+# Switches to pass to esphome. See 'esphomeyaml --help' for
+# a description of the possible switches.
+# For now the only usable option for dashboard mode is --port
+#
+ESPHOME_OPTS=""
