54ffc21344106727c8d62a849539c7b21067ccd6
Andreas Billmeier (@Home/mbp) <b@edevau.net>
2022-06-01 01:21:14 +0200

    remove olds

--- app-misc/homeassistant/homeassistant-full/files/homeassistant.conf.d
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2018 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-
-# /etc/conf.d/homeassistant: configuration for /etc/init.d/homeassistant
-
-# Switches to pass to hass. See 'hass --help' for
-# a description of the possible switches.
-#
-HASS_OPTS=""
